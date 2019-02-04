6401349716f2ef8bbdf374a4d2d8aaea4329a987
andresdlc <andresdlc@users.noreply.github.com>
2019-02-04 15:41:14 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { Operation } from './entities/operation.entity';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 
 
