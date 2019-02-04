c0d3c4163fcb7a0828202e79f08a32f8f96da009
andresdlc <andresdlc@users.noreply.github.com>
2019-02-04 13:47:31 -0500

    Add files via upload

---
+++ src/app/services/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Result } from '../entities/hello-world.entity';
+import { Operation } from '../entities/operation.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AppService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getOperationResult(operation: Operation): Observable<Result> {
+    //return this.httpClient.post("http://107.23.58.217/hello/calculadora", operation);
+    return this.httpClient.post("http://107.23.58.217/calculadora/hello/calculadora", operation);
+  }
+}
